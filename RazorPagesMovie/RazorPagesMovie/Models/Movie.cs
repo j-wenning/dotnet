94d9404e02d555c2a402ab6ebd05bddf67d4298e
j-wenning <wenning.justin@gmail.com>
2020-10-31 14:41:08 -0700

    Updated movie model frontend validation

--- RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
@@ -7,12 +7,13 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+        [StringLength(60, MinimumLength = 3), Required]
         public string Title { get; set; }
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
         public DateTime Release { get; set; }
-        [Column(TypeName = "real")]
+        [Range(1, 100), DataType(DataType.Currency), Column(TypeName = "real")]
         public float Price { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5), Required]
         public string Rating { get; set; }
     }
 }
