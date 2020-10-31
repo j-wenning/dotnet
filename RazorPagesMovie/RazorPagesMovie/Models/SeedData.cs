b8ed6852f22723167f169cd9d1b91752738ce254
j-wenning <wenning.justin@gmail.com>
2020-10-30 18:14:50 -0700

    Updated seed data for db to include ratings

--- RazorPagesMovie/RazorPagesMovie/Models/SeedData.cs
+++ RazorPagesMovie/RazorPagesMovie/Models/SeedData.cs
@@ -20,25 +20,29 @@ namespace RazorPagesMovie.Models
                     {
                         Title = "When Harry Met Sally",
                         Release = DateTime.Parse("1989-2-12"),
-                        Price = 7.99f
+                        Price = 7.99f,
+                        Rating = "R"
                     },
                     new Movie
                     {
-                        Title = "Ghostbusters ",
+                        Title = "Ghostbusters",
                         Release = DateTime.Parse("1984-3-13"),
-                        Price = 8.99f
+                        Price = 8.99f,
+                        Rating = "PG"
                     },
                     new Movie
                     {
                         Title = "Ghostbusters 2",
                         Release = DateTime.Parse("1986-2-23"),
-                        Price = 9.99f
+                        Price = 9.99f,
+                        Rating = "PG"
                     },
                     new Movie
                     {
                         Title = "Rio Bravo",
                         Release = DateTime.Parse("1959-4-15"),
-                        Price = 3.99f
+                        Price = 3.99f,
+                        Rating = "U"
                     }
                 );
                 context.SaveChanges();
